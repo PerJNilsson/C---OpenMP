fc1767b07f584703ab406621b113f87b3583b9a9
perjnilsson <per90nilsson@gmail.com>
2018-10-14 11:35:43 +0200

    Added dynamic scheduling, almost twice as fast

--- cell_distance.c
+++ cell_distance.c
@@ -6,18 +6,23 @@
 #include <float.h>
 // Define global variables
 FILE *fp_cell;
+FILE * fp_cell_init;
 double ** coordinateMatrix;
 double * cellPointer;
 unsigned long long * distanceArray;
 // Initialize funtions
 double sqrt(double x);
-
+long count_lines();
 
 int main(int argc, char *argv[]){
 
-  // size = hardcoded number since we know how many elements it is in the file
-  char file_name[] = "cell.txt";
-  int size = 100000;
+  char file_name[] = "cells";
+  printf("\nFile name is: %s\n\n", file_name);
+  fp_cell = fopen(file_name, "r");
+  long size = count_lines();
+  fclose(fp_cell);
+  
+  //long size = 100000;
   
   short maximumLengthResolution = 3466;
   cellPointer = malloc(sizeof(double) *3*size);
@@ -35,7 +40,7 @@ int main(int argc, char *argv[]){
 
    // Initialize  the number of threads and the thread ID.
   int nThreads;
-
+  
   nThreads = atoi(argv[1]+2); if (nThreads == 0 || nThreads> 112) {printf("Wrong argument please write -t#, # being a number"); exit(-1);}
   omp_set_num_threads(nThreads);
   
@@ -49,9 +54,9 @@ int main(int argc, char *argv[]){
       coordinateMatrix[i][2] = l;  
     }
   fclose(fp_cell);  
-
+  
   // Initializing parallel threading with reduction as stated above
-#pragma omp parallel for reduction(+:distanceArray[:maximumLengthResolution])
+#pragma omp parallel for schedule(dynamic,1) reduction(+:distanceArray[:maximumLengthResolution])
   for (int i = 0; i<size; i++){
     for (int j = 0; j<i; j++){
       int tmpShortDistance =0;
@@ -68,10 +73,26 @@ int main(int argc, char *argv[]){
   unsigned long long sum = 0;
   
   for (int i = 0; i<maximumLengthResolution; i++) {
-     if (distanceArray[i] != 0) {
-      printf("%.2f  %d\n", i/100.0, distanceArray[i]);
-      }
-  }
+    if (distanceArray[i] != 0 && i < 1000) {
+      printf("0%.2f  %d\n", i/100.0, distanceArray[i]);
+    }
+    else if (distanceArray[i] !=0) {
+      printf("%.2f %d\n", i/100.0, distanceArray[i]);
+    }
+    }
   
   return 0;
+}
+
+long count_lines(){
+
+  long tmpSize=0;
+  char ch;
+  do{
+    ch = fgetc(fp_cell);
+    if(ch == '\n'){
+      tmpSize++;
+    }
+  } while (ch != EOF);
+  return tmpSize;    
 }
