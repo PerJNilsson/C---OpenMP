583c1eec2ce1a4b4d53e9e53ad27a035dae3e72d
perjnilsson <per90nilsson@gmail.com>
2018-10-18 16:47:18 +0200

    Added file with coordinate, and the right answer

--- cell_distance.c
+++ cell_distance.c
@@ -5,35 +5,32 @@
 #include <omp.h>
 // Define global variables
 FILE *fp_cell;
-double ** coordinateMatrix;
 double * cellPointer;
-int * distanceArray;
+unsigned long long * distanceArray;
+double ** coordinateMatrix;
 // Initialize funtions
 double sqrt(double x);
 
 int main(int argc, char *argv[]){
 
-  char file_name[] = "cells";
+  if (argc > 2){
+    printf("Wrong arguments, please write  -t#, # being number of threads");
+    exit(-1);
+      }
+  char file_name[] = "cell_e5";
 
   fp_cell = fopen(file_name, "r");
-  fseek(fp_cell,0, SEEK_END);
+  fseek(fp_cell, 0, SEEK_END);
   unsigned long long size = ftell(fp_cell)/24;
   fseek(fp_cell, 0, SEEK_SET);
   fclose(fp_cell);
   
   short maximumLengthResolution = 3466;
-
-  double (*coordinateMatrix)[3] = malloc(sizeof(double)*size*3);
-  /*
-  cellPointer = malloc(sizeof(double) *3*size);
-  coordinateMatrix = malloc(sizeof(double) *size);
-  for (size_t i=0, j=0; i<size; i++, j+=3){
-    coordinateMatrix[i] = cellPointer + j; 
-    }*/
- 
-  // Each distance with 2 decimals can be saved as a short by multiplying the number with 100..
-  // For each distance the corresponding elemten in distanceMatrix will +=1
-  // Using sizeof(unsigned long long) if it happens that all are at the same point and we have 2^32 coordinates.
+  double (*coordinateMatrix)[3] = malloc(sizeof(double)*size*3); // Allocating space for the matrix where the coordinates are stored.
+  
+  /* Each distance with 2 decimals can be saved as a short by multiplying the number with 100..
+     For each distance the corresponding elemten in distanceMatrix will +=1
+     Using sizeof(unsigned long long) if it happens that all are at the same point and we have 2^32 coordinates.*/
   distanceArray =calloc(maximumLengthResolution,sizeof(unsigned long long));
 
    // Initialize the number of threads, where #threads will be the interger after -t in argv[1], else it exits the program.
@@ -42,45 +39,43 @@ int main(int argc, char *argv[]){
   omp_set_num_threads(nThreads);
 
 
+fp_cell = fopen(file_name, "r");
+
   // Unparallelized reading of a file. 
-  fp_cell = fopen(file_name, "r");
   for (int i = 0; i < size; i++) {
-      double n,m,l;
-      // fscanf checks each row which is the coordinate for each point
-      fscanf(fp_cell, "%lf %lf %lf", &n, &m, &l);
-      coordinateMatrix[i][0] = n;
-      coordinateMatrix[i][1] = m;
-      coordinateMatrix[i][2] = l;  
-    }
+    double n,m,l;
+    // fscanf checks each row which is the coordinate for each point
+    fscanf(fp_cell, "%lf %lf %lf", &n, &m, &l);
+    coordinateMatrix[i][0] = n;
+    coordinateMatrix[i][1] = m;
+    coordinateMatrix[i][2] = l;  
+      }
   fseek(fp_cell, 0, SEEK_SET);
   fclose(fp_cell);  
   
-  /* Initializing parallel threading with reduction meaning the shared array distanceArray will have all its elements from each thread summed up after all threads are finished with their computations.
-The dynamic scheduling will divide the payload for each thread equally, which is necessary since our first row barely have any comupations and our last rows have a lot of computations. */
+  /* Initializing parallel threading with reduction meaning the shared array distanceArray will have all its elements from each thread summed up          after all threads are finished with their computations.
+     The dynamic scheduling will divide the payload for each thread equally, which is necessary since our first row barely have any comupations and       our last rows have a lot of computations. */
 #pragma omp parallel for schedule(dynamic) reduction(+:distanceArray[:maximumLengthResolution])
   for (int i = 0; i<size; i++){
     for (int j = 0; j<i; j++){
-      int tmpShortDistance =0;
+      short tmpShortDistance = 0;
       tmpShortDistance =0.5+100*sqrt(((coordinateMatrix[i][0]-coordinateMatrix[j][0]) * (coordinateMatrix[i][0]-coordinateMatrix[j][0]))+ \
 		       ((coordinateMatrix[i][1]-coordinateMatrix[j][1]) * (coordinateMatrix[i][1]-coordinateMatrix[j][1]))+\
-		       ((coordinateMatrix[i][2]-coordinateMatrix[j][2]) * (coordinateMatrix[i][2]-coordinateMatrix[j][2])));
-      
+		       ((coordinateMatrix[i][2]-coordinateMatrix[j][2]) * (coordinateMatrix[i][2]-coordinateMatrix[j][2])));      
       {
 	distanceArray[tmpShortDistance] +=1;
-
       }
     }
   }
-  unsigned long long sum = 0;
-  
+ 
   for (int i = 0; i<maximumLengthResolution; i++) {
     if (distanceArray[i] != 0 && i < 1000) {
       printf("0%.2f %d\n", i/100.0, distanceArray[i]);
     }
-    else if (distanceArray[i] !=0/* && i >= 1000*/) {
+    else if (distanceArray[i] !=0 && i >= 1000) {
       printf("%.2f %d\n", i/100.0, distanceArray[i]);
     }
   }
-  
+
   return 0;
 }
